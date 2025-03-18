e5665b54c22214f9fc82f6301eb37ac7ce7e5132
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2025-03-17 21:58:18 -0300

    Create Frontend | Build :open_file_folder:

---
+++ Projeto_calendario/frontend/src/components/EventForm.vue
@@ -0,0 +1,11 @@
+<template>
+    
+    <div id="ContainerForm">
+
+        <form>
+            
+        </form>
+
+    </div>
+
+</template>
---
+++ Projeto_calendario/frontend/src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="HomeMonthView">
+    
+    <header>
+      <section id="ChoiceDate">
+        <div id="MonthChoide">
+          <ul>
+            <li>Jan</li>
+            <li>Fev</li>
+            <li>Mar</li>
+            <li>Abr</li>
+            <li>Mai</li>
+            <li>Jun</li>
+            <li>Jul</li>
+            <li>Ago</li>
+            <li>Set</li>
+            <li>Out</li>
+            <li>Nov</li>
+            <li>Dez</li>
+          </ul>
+        </div>
+        <div id="YearDate">
+          <ul><!-- fazer loop para a cada 5 anos a frente do atual -->
+      
+          </ul>
+        </div>
+      </section>
+
+      <section id="MontSelected">
+        <h1>Março</h1>
+        <h1>2025</h1>
+      </section>
+
+      <aside>
+        <p>Calendário</p>
+      </aside>
+
+    </header>
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
