7c2b5c2cb2b3601fd4f61cc50aa546a37f960cbb
Valmar-dev <valmarpontedev@gmail.com>
2025-03-22 20:38:30 -0300

    criando a rota DELETE

--- Projeto_calendario/backend/evento/urls.py
+++ Projeto_calendario/backend/evento/urls.py
@@ -1,8 +1,9 @@
 # evento/urls.py
 from django.urls import path
-from .views import EventoCadastro, EventoList
+from .views import EventoCadastro, EventoList, EventoDelete
 
 urlpatterns = [
     path('cadastro/', EventoCadastro.as_view(), name='evento-cadastro'),
     path('eventos/', EventoList.as_view(), name='retorna-eventos'),
+    path('delete/<int:pk>/', EventoDelete.as_view(), name='deleta-eventos'),
 ]
--- Projeto_calendario/backend/evento/views.py
+++ Projeto_calendario/backend/evento/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Evento
 from .serializers import EventoSerializer
+from rest_framework.exceptions import NotFound
 
 # cadastro de eventos
 class EventoCadastro(APIView):  
@@ -23,9 +24,20 @@ class EventoCadastro(APIView):
         }, status=status.HTTP_400_BAD_REQUEST)
 
 
-# retornando dados do baco para o front
+# retornando dados GET do baco para o front
 class EventoList(APIView):
     def get(self, request):
         eventos = Evento.objects.all()  
         serializer = EventoSerializer(eventos, many=True)
         return Response(serializer.data)  # Retorno tudo em formato JSON
+    
+
+# função DELETE
+class EventoDelete(APIView):
+    def delete(self, request, pk):
+        try:
+            evento = Evento.objects.get(id=pk)  # Busca o evento pelo id
+            evento.delete()  # Deleta o evento
+            return Response({"message": "Evento deletado com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
+        except Evento.DoesNotExist:
+            raise NotFound(detail="Evento não encontrado")
