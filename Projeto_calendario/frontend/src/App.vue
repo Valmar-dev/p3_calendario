7aac32a4043cc7329019ddb3a95144a351e06248
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2025-03-22 00:09:08 -0300

    Base Styles | Feat :pencil2:

--- Projeto_calendario/frontend/src/App.vue
+++ Projeto_calendario/frontend/src/App.vue
@@ -11,6 +11,22 @@
   color: #2c3e50;
 }
 
+:root{
+
+  --color-main00:#f5f5f5;
+  --color-main01:#cbe5e7;
+  --color-main02:#7fcec3;
+  --color-main03:#289482;
+  --color-main04:#165e52;
+
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 nav {
   padding: 30px;
 }
@@ -23,4 +39,35 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+header{
+  background-color: var(--color-main01);
+  padding: 14px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+
+}
+
+section#dataEscolhida{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.home-view{
+  position: relative;
+}
+
+footer{
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  background-color: var(--color-main01);
+  padding: 3px 0;
+}
+
 </style>
